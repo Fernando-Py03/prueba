34c91fe19978a534f20a48c3962e1730fb366c74
Fernando03 <esquiviafernando33@gmail.com>
2025-06-19 14:34:09 -0500

    se hicieron dos funciones una de sumar y otra restar

--- suma.py
+++ suma.py
@@ -1,3 +1,25 @@
 def suma(numero1, numero2):
     return print("el resultado de suma es: ", (numero1+numero2))
 
+def resta(numero1, numero2):
+    return print("El resultado de la resta es: " (numero1-numero2))
+
+while True:
+    try:
+        numero1 = int(input("Por favor ingresa el primer numero: "))
+        numero2 = int(input("Por favor ingresa el segundo numero: "))
+        if numero1 > 0 and numero2 > 0:
+            break
+        else: print("por favor ingresa un numero positivo")
+    except: print("Por favor ingresa un numero")
+    
+while True:
+    print("1- sumar  2- restar")
+    opcion = int(input("por favor dijita 1 0 2"))
+    if opcion == 1:
+        suma(numero1, numero2)
+        break
+    elif opcion == 2:
+        resta(numero1, numero2)
+        break
+    else: print("eligio una opcion equivocada!")
