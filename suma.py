86716c306b6ecd2185fe7ae552fc5da3eb17720f
Fernando03 <esquiviafernando33@gmail.com>
2025-06-19 15:04:11 -0500

    Se cambio el nombre de la variable

--- suma.py
+++ suma.py
@@ -1,14 +1,14 @@
-def suma(numero1, numero2):
-    return print("el resultado de suma es: ", (numero1+numero2))
+def suma(numero3, numero2):
+    return print("el resultado de suma es: ", (numero3+numero2))
 
-def resta(numero1, numero2):
-    return print("El resultado de la resta es: " (numero1-numero2))
+def resta(numero3, numero2):
+    return print("El resultado de la resta es: " (numero3-numero2))
 
 while True:
     try:
-        numero1 = int(input("Por favor ingresa el primer numero: "))
+        numero3 = int(input("Por favor ingresa el primer numero: "))
         numero2 = int(input("Por favor ingresa el segundo numero: "))
-        if numero1 > 0 and numero2 > 0:
+        if numero3 > 0 and numero2 > 0:
             break
         else: print("por favor ingresa un numero positivo")
     except: print("Por favor ingresa un numero")
@@ -17,9 +17,11 @@ while True:
     print("1- sumar  2- restar")
     opcion = int(input("por favor dijita 1 0 2"))
     if opcion == 1:
-        suma(numero1, numero2)
+        suma(numero3, numero2)
         break
     elif opcion == 2:
-        resta(numero1, numero2)
+        resta(numero3, numero2)
         break
     else: print("eligio una opcion equivocada!")
+    
+print("hola fernando")
